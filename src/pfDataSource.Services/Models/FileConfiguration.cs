b6844a5ba2e5e9dbbb7468c2a9e5da4b50697bc4
Chris Hogg <chris.hogg@rezare.com>
2023-08-10 12:07:37 +1200

    feat: added regexes to validate dir path and cron expression

--- src/pfDataSource.Services/Models/FileConfiguration.cs
+++ src/pfDataSource.Services/Models/FileConfiguration.cs
@@ -6,23 +6,16 @@ namespace pfDataSource.Services.Models
 	public class FileConfiguration
 	{
         [Required]
-        [RegularExpression(@"^([a-zA-Z]:\\)([-\u4e00-\u9fa5\w\s.()~!@#$%^&()\[\]{}+=]+\\)*$ ")]
+        [RegularExpression(@"(\/.*|[a-zA-Z]:\\(?:([^<>:""\/\\|?*]*[^<>:""\/\\|?*.]\\|..\\)*([^<>:""\/\\|?*]*[^<>:""\/\\|?*.]\\?|..\\))?)", ErrorMessage = "Please provide a valid path. Windows paths must start with the drive, eg C:.")]
         public string Path { get; set; }
         public string FileType { get; set; }
 		public int SubmissionDelay { get; set; } = 0;
 		public bool WatchDirectory { get; set; }
 
         [Required]
-        [RegularExpression(@"^(?#minute)(\*|(?:[0-9]|(?:[1-5][0-9]))(?:(?:\-[0-9]|\-(?:[1-5][0-9]))?|
-(?:\,(?:[0-9]|(?:[1-5][0-9])))*)) (?#hour)(\*|(?:[0-9]|1[0-9]|2[0-3])
-(?:(?:\-(?:[0-9]|1[0-9]|2[0-3]))?|(?:\,(?:[0-9]|1[0-9]|2[0-3]))*)) 
-(?#day_of_month)(\*|(?:[1-9]|(?:[12][0-9])|3[01])(?:(?:\-(?:[1-9]|
-(?:[12][0-9])|3[01]))?|(?:\,(?:[1-9]|(?:[12][0-9])|3[01]))*)) (?#month)(\*|
-(?:[1-9]|1[012]|JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC)(?:(?:\-(?:[1-9]|
-1[012]|JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC))?|(?:\,(?:[1-9]|1[012]|
-JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC))*)) (?#day_of_week)(\*|
-(?:[0-6]|SUN|MON|TUE|WED|THU|FRI|SAT)(?:(?:\-(?:[0-6]|SUN|MON|TUE|WED|THU|FRI|SAT))?|
-(?:\,(?:[0-6]|SUN|MON|TUE|WED|THU|FRI|SAT))*))$", ErrorMessage = "Cron expression must be in the cron POSIX format.")]
+      
+        [RegularExpression(@"^((\*|\?|\d+((\/|\-){0,1}(\d+))*)\s*){5}$", ErrorMessage = "Cron expression must be in the POSIX format.")]
+      
         public string CronExpression { get; set; }
 	}
 }
